4143d7f38061f9e12dbf11726928107568420ce8
juarezadolfo <juarezadolfo@hotmail.com>
2017-12-06 14:17:10 -0800

    adolfo's commit1

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,19 @@
+//Create a click event on the Search Button
 
+$("#searchBtn").click (function () {
+    //Get user input from each section.
+    //Create an object userInput.
+    //Inside the userInput store all the parameter data.
+    var userInput = {
+        search: $("#search").val(),
+        records: $(".records").val(),
+        start: $("#start").val(),
+        end: $("#end").val(),
+        
+    }
+
+
+});
 var APIKey = "b9f91d369ff59547cd47b931d8cbc56b:0:74623931";
 
 var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + APIKey;
@@ -12,5 +27,5 @@ $.ajax({
     console.log(response);
 
 
-    $(".docs").text("This is your doc: " + JSON.stringify(response.response.docs));
+    $(".panel-body").text("This is your doc: " + JSON.stringify(response.response.docs));
 });
